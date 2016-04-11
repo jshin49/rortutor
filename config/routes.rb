d3bc678c08194222444452df37fc6d7a53bbffb2
jshin49 <jshin49@gmail.com>
2016-04-11 23:29:09 +0200

    Comment Functionality finished

--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,9 @@ Rails.application.routes.draw do
   #     end
   #   end
   Rails.application.routes.draw do 
-    resources :articles
+    resources :articles do
+      resources :comments
+    end
     root 'welcome#index'
   end
 
